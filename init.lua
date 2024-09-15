af405b525192e73a746c87ddf89cc65dba77f192
kiddde <pwrthr@gmail.com>
2024-09-15 21:29:11 +0200

    indent

--- init.lua
+++ init.lua
@@ -160,6 +160,7 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.autoindent = true
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
